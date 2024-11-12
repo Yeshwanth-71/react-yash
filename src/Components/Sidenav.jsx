47226982f4729f2f53d011966c067e6872137dfe
Yeshwanth <reddyyeshwanth720@gmail.com>
2024-11-12 21:26:01 +0530

    new

--- src/Components/Sidenav.jsx
+++ src/Components/Sidenav.jsx
@@ -7,10 +7,6 @@ import { BsEarbuds } from "react-icons/bs";
 import { BiSolidFridge } from "react-icons/bi";
 import { IoMdMenu, IoMdClose } from "react-icons/io";
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import { FaMobileAlt, FaTv, FaGithub, FaUser } from "react-icons/fa";
-import { IoMdLaptop, IoMdSettings, IoMdMenu, IoMdClose } from "react-icons/io";
-import { BsEarbuds } from "react-icons/bs";
-import { CgSmartHomeRefrigerator } from "react-icons/cg";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Sidenav() {
